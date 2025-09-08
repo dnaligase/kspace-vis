b3eda6a9425a26b5511ed1adf14a9c55b75ebeb5
dnaligase <gric.gosha@ya.ru>
2025-09-08 22:24:14 +0300

    memory optimize processing.py

--- processing.py
+++ processing.py
@@ -8,6 +8,7 @@ from typing import cast
 
 
 def process_image(path: str):
+    # TODO: variable namings improve
     image = Image.open(path)
     # Resize the image
     image = image.resize((86, 86))
@@ -19,30 +20,29 @@ def process_image(path: str):
 
     kl = list(product(np.arange(len(img)), repeat=2))
     coefs = np.zeros(len(kl), dtype=np.complex64)
-    bars = np.zeros((rows, cols, len(kl)), dtype=np.complex64)
+    images = np.zeros((rows, cols, len(kl)), dtype=np.float32)
 
     i = np.arange(rows).reshape(-1, 1)
     j = np.arange(cols).reshape(1, -1)
 
     for _, (k, l) in enumerate(tqdm(kl, desc='Applying manual DFT...')):
         exponent = -2j * np.pi * ((k * i / rows) + (l * j / cols))
-        bars[:, :, _] = np.exp(exponent)
-        coefs[_] = np.sum(img * np.exp(exponent))
+        exp = np.exp(exponent)
+        coefs[_] = np.sum(img * exp)
+        images[:, :, _] = np.real(coefs[_] * exp)
 
-    images = np.real(bars * coefs)
     y = fft2(img)
     fourier_im = fftshift(y)
 
     X_std = (images - images.min(axis=(0, 1))) / (images.max(axis=(0, 1)) - images.min(axis=(0, 1)) + 1e-5)
-    X_scaled = X_std * (255 - 0) + 0
+    X_std *= (255 - 0) + 0
 
-    X_scaled[:, :, 0] = img.mean()
+    X_std[:, :, 0] = img.mean()
 
     # data
     grid_data = np.log2(np.abs(fourier_im))
     idx_mat = np.arange(grid_data.shape[0] * grid_data.shape[1]).reshape(grid_data.shape)
     idx_mat_shifted = fftshift(idx_mat)
 
-    imgs = X_scaled[:, :, idx_mat_shifted.flatten()]
-
-    return images, imgs, idx_mat, idx_mat_shifted, grid_data
+    X_std = X_std[:, :, idx_mat_shifted.flatten()]
+    return images, X_std, idx_mat, idx_mat_shifted, grid_data
